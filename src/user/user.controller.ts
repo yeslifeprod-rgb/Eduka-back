b17f18256d0c9c94b05e7b387e1d3829ee8e3bb5
yeslifeprod-rgb <228542534+yeslifeprod-rgb@users.noreply.github.com>
2024-06-26 17:02:32 +0200

    "Edit-User-Controller"

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,18 +1,19 @@
 import { Controller, Post, Body, HttpCode, HttpStatus, UseGuards, Put, Param } from '@nestjs/common';
+
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { ProfileService } from './profile.service';
 import { UpdateModifProfileDto } from './dto/update-modifprofile.dto';
-import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 
 
   @Controller('user')
 export class UserController {
-  constructor(private readonly modifProfileService: ProfileService) { }
+  constructor(private readonly ProfileService: ProfileService) { }
 
   @UseGuards(JwtAuthGuard)
   @Put('update/:id')
   async updateUserProfile(@Param('id') id: string, @Body() updateModifProfileDto: UpdateModifProfileDto) {
-    return this.modifProfileService.updateUserProfile(+id, updateModifProfileDto);
+    return this.ProfileService.updateUserProfile(+id, updateModifProfileDto);
   }
 }
 
